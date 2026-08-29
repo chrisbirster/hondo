3f45a0a3bd0435f60da03dc903d76b282d8afc0a
Chris Birster <christopher.birster@gmail.com>
2026-08-29 19:52:11 -0400

    feat: add Solid 2 package entrypoint

---
+++ packages/solid/src/index.ts
@@ -0,0 +1,7 @@
+export { createRenderer as createUniversalRenderer } from '@solidjs/universal';
+export { createSignal } from 'solid-js';
+
+export const solid2Baseline = {
+  core: '2.0.0-rc.1',
+  universal: '2.0.0-rc.0',
+} as const;
