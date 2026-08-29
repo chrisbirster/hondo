a5c0a8a37de07ec99cd3d077429c10e1fef1a0ff
Chris Birster <christopher.birster@gmail.com>
2026-08-29 19:51:15 -0400

    feat: add Hondo Zig root module

---
+++ zig/src/root.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub const version = "0.1.0-alpha.0";
+
+pub const RuntimeBoundary = enum {
+    solid,
+    hondo,
+    zig,
+};
+
+test "foundation exposes a version" {
+    try std.testing.expect(version.len > 0);
+}
