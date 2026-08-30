8536ee7f673adac7c90064ec715f06317c769137
Chris Birster <christopher.birster@gmail.com>
2026-08-30 12:00:50 -0400

    feat: add terminal module

---
+++ zig/src/terminal/mod.zig
@@ -0,0 +1,9 @@
+pub const input = @import("input.zig");
+pub const control = @import("control.zig");
+pub const session = @import("session.zig");
+
+test {
+    _ = input;
+    _ = control;
+    _ = session;
+}
